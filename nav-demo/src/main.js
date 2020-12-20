b7682a6fcc3317670e9d6e91a8e38913eb276db0
meakle <meakle@qq.com>
2020-12-20 21:38:59 +0800

    fix: 修复三个bug,1.进入搜索框打字会触发快捷进入 2.添加的标签无法使用快捷进入 3.删除最后一个标签会把添加的list删除

--- nav-demo/src/main.js
+++ nav-demo/src/main.js
@@ -16,6 +16,9 @@ const hashMap = xObject || [{
     }
 ]
 
+
+
+// 把url的格式改成我们需要的
 const removeFormart = (url) => {
     return url.replace('https://', '')
         .replace('http://', '')
@@ -25,10 +28,11 @@ const removeFormart = (url) => {
 
 let render = () => {
     // 每一次render之前先把除了最后一个节点的所有节点删除一下
-    $siteList.find('li:not(.last)').remove();
+    $siteList.find('li:not(.btn-list)').remove();
 
 
     hashMap.forEach((node, index) => {
+        // 遍历哈希表中存下的值
         const $li = $(`
         <li>
                 <div class="site">
@@ -50,23 +54,40 @@ let render = () => {
         `).appendTo($siteList);
         $btnList.insertAfter($li);
 
+        // 点击跳转
         $li.on('click', () => {
             window.open(node.url)
         })
 
+        // 关闭
         $li.on('click', '.close', (e) => {
             e.stopPropagation() // 阻止冒泡
             hashMap.splice(index, 1);
-            render()
+            console.log(hashMap);
+            render();
         })
     })
 }
 
+document.addEventListener('keypress', (e) => {
+    const {
+        key
+    } = e;
+
+    // 判断一下, 如果是
+    if (e.target !== document.querySelector('#searchInput')) {
+        for (let i = 0; i < hashMap.length; i++) {
+            if (hashMap[i].logo.toLowerCase() === key) {
+                window.open(hashMap[i].url);
+            }
+        }
+    }
+
+});
 
 
-// 自己先初始化一遍
+// 添加网址之前, 自己先初始化一遍
 render();
-
 document.querySelector('.btn-addsite').addEventListener('click', () => {
     let url = window.prompt('你要输入的网址是个啥');
 
@@ -75,28 +96,18 @@ document.querySelector('.btn-addsite').addEventListener('click', () => {
     }
 
     hashMap.push({
-        logo: url[0],
+        logo: removeFormart(url)[0],
+        // logo: url[0],
         logoType: 'text',
         url: url,
-    })
+    });
 
     render();
 });
 
+
+// 在离开该页面的时候储存
 window.onbeforeunload = () => {
     const string = JSON.stringify(hashMap);
     localStorage.setItem('x', string);
 }
-
-
-$(document).on('keypress', (e) => {
-    const {
-        key
-    } = e;
-
-    for (let i = 0; i < hashMap.length; i++) {
-        if (hashMap[i].logo.toLowerCase() === key) {
-            window.open(hashMap[i].url);
-        }
-    }
-})
